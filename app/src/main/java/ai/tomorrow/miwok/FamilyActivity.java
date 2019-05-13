d292d04d2bfb7550b3949706d7812106d24d7545
Yini <ydxb7@mail.missouri.edu>
2019-05-13 15:29:35 -0700

    add all empty activities

--- app/src/main/java/ai/tomorrow/miwok/FamilyActivity.java
+++ app/src/main/java/ai/tomorrow/miwok/FamilyActivity.java
@@ -3,11 +3,11 @@ package ai.tomorrow.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class FamilyMembersActivity extends AppCompatActivity {
+public class FamilyActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_family_members);
+        setContentView(R.layout.activity_family);
     }
 }
--- app/src/main/java/ai/tomorrow/miwok/MainActivity.java
+++ app/src/main/java/ai/tomorrow/miwok/MainActivity.java
@@ -20,6 +20,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import ai.tomorrow.miwok.R;
 
@@ -32,15 +33,42 @@ public class MainActivity extends AppCompatActivity {
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
 
-        NumbersClickListener clickListener = new NumbersClickListener();
-
         TextView numbers = (TextView) findViewById(R.id.numbers);
-        numbers.setOnClickListener(clickListener);
+        numbers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, NumbersActivity.class);
+                startActivity(i);
+            }
+        });
+
+        TextView family = (TextView) findViewById(R.id.family);
+        family.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, FamilyActivity.class);
+                startActivity(i);
+            }
+        });
+
+        TextView colors = (TextView) findViewById(R.id.colors);
+        colors.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, ColorsActivity.class);
+                startActivity(i);
+            }
+        });
+
+        TextView phrases = (TextView) findViewById(R.id.phrases);
+        phrases.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, PhrasesActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 
-    public void openNumbersList(View view)
-    {
-        Intent i = new Intent(this, NumbersActivity.class);
-        startActivity(i);
-    }
 }
