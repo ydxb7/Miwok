286f6adb477334427ad43b72cabe6336beea06f2
Yini <ydxb7@mail.missouri.edu>
2019-05-16 03:21:30 -0700

    audio focus

--- app/src/main/java/ai/tomorrow/miwok/NumbersActivity.java
+++ app/src/main/java/ai/tomorrow/miwok/NumbersActivity.java
@@ -1,8 +1,10 @@
 package ai.tomorrow.miwok;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -18,24 +20,50 @@ public class NumbersActivity extends AppCompatActivity {
             releaseMediaPlayer();
         }
     };
+    private AudioManager mAudioManager;
+    private AudioManager.OnAudioFocusChangeListener mOnAudioFocusChangeListener =
+            new AudioManager.OnAudioFocusChangeListener() {
+                public void onAudioFocusChange(int focusChange) {
+                    if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT) {
+                        // Pause playback because your Audio Focus was temporarily stolen, but will be back soon. i.e. for a phone call
+                        mPlayer.pause();
+                        mPlayer.seekTo(0); // start from the begining
+                    } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+                        // Stop playback, because you lost the Audio Focus.
+                        releaseMediaPlayer();
+                    } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
+                        // Lower the volume or pause, because something else is also playing audio over you.
+                        mPlayer.pause();
+                        mPlayer.seekTo(0); // start from the begining
+                    } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+                        // Resume playback, because you hold the Audio Focus again!
+                        mPlayer.start();
+                    }
+                }
+            };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.word_list);
 
-        final ArrayList<Word> numbers = new ArrayList<Word>();
-        numbers.add(new Word("one", "lutii", R.drawable.number_one, R.raw.number_one));
-        numbers.add(new Word("two", "otiiko", R.drawable.number_two, R.raw.number_two));
-        numbers.add(new Word("three", "tolookosu", R.drawable.number_three, R.raw.number_three));
-        numbers.add(new Word("four", "oyyisa", R.drawable.number_four, R.raw.number_four));
-        numbers.add(new Word("five", "massokka", R.drawable.number_five, R.raw.number_five));
-        numbers.add(new Word("six", "temmokka", R.drawable.number_six, R.raw.number_six));
-        numbers.add(new Word("seven", "kenekaku", R.drawable.number_seven, R.raw.number_seven));
-        numbers.add(new Word("eight", "kawinta", R.drawable.number_eight, R.raw.number_eight));
-        numbers.add(new Word("nine", "wo'e", R.drawable.number_nine, R.raw.number_nine));
-        numbers.add(new Word("ten", "na'aacha", R.drawable.number_ten, R.raw.number_ten));
+        mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
 
-        WordAdapter itemsAdapter = new WordAdapter(this, R.layout.list_item, numbers, R.color.category_numbers);
+
+        final ArrayList<Word> words = new ArrayList<Word>();
+        words.add(new Word("one", "lutii", R.drawable.number_one, R.raw.number_one));
+        words.add(new Word("two", "otiiko", R.drawable.number_two, R.raw.number_two));
+        words.add(new Word("three", "tolookosu", R.drawable.number_three, R.raw.number_three));
+        words.add(new Word("four", "oyyisa", R.drawable.number_four, R.raw.number_four));
+        words.add(new Word("five", "massokka", R.drawable.number_five, R.raw.number_five));
+        words.add(new Word("six", "temmokka", R.drawable.number_six, R.raw.number_six));
+        words.add(new Word("seven", "kenekaku", R.drawable.number_seven, R.raw.number_seven));
+        words.add(new Word("eight", "kawinta", R.drawable.number_eight, R.raw.number_eight));
+        words.add(new Word("nine", "wo'e", R.drawable.number_nine, R.raw.number_nine));
+        words.add(new Word("ten", "na'aacha", R.drawable.number_ten, R.raw.number_ten));
+
+        WordAdapter itemsAdapter = new WordAdapter(this, R.layout.list_item, words, R.color.category_numbers);
         final ListView listView = (ListView) findViewById(R.id.list);
         listView.setAdapter(itemsAdapter);
 
@@ -43,19 +71,37 @@ public class NumbersActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 releaseMediaPlayer();
-                mPlayer = MediaPlayer.create(NumbersActivity.this, numbers.get(position).getAudioResourceId());
-                mPlayer.start();
-                mPlayer.setOnCompletionListener(mCompletionListener);
+                // Request audio focus for playback
+                int result = mAudioManager.requestAudioFocus(mOnAudioFocusChangeListener,
+                        // Use the music stream.
+                        AudioManager.STREAM_MUSIC,
+                        // Request temporary focus.
+                        AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+
+                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                    // Start playback
+                    mPlayer = MediaPlayer.create(NumbersActivity.this, words.get(position).getAudioResourceId());
+                    mPlayer.start();
+                    mPlayer.setOnCompletionListener(mCompletionListener);
+                }
             }
         });
 
-        Log.v("NumbersActivity", "index 2 element in numbers: " + numbers.get(2));
+        Log.v("NumbersActivity", "index 2 element in words: " + words.get(2));
     }
 
-    private void releaseMediaPlayer(){
-        if(mPlayer != null){
+    private void releaseMediaPlayer() {
+        if (mPlayer != null) {
             mPlayer.release();
             mPlayer = null;
+            mAudioManager.abandonAudioFocus(mOnAudioFocusChangeListener); // abandon focus
         }
+    }
+
+    @Override
+    protected void onStop() {
+        // app is hide
+        super.onStop();
+        releaseMediaPlayer();
     }
 }
--- app/src/main/java/ai/tomorrow/miwok/PhrasesActivity.java
+++ app/src/main/java/ai/tomorrow/miwok/PhrasesActivity.java
@@ -1,5 +1,7 @@
 package ai.tomorrow.miwok;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -18,25 +20,48 @@ public class PhrasesActivity extends AppCompatActivity {
             releaseMediaPlayer();
         }
     };
+    private AudioManager mAudioManager;
+    private AudioManager.OnAudioFocusChangeListener mOnAudioFocusChangeListener =
+            new AudioManager.OnAudioFocusChangeListener() {
+                public void onAudioFocusChange(int focusChange) {
+                    if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT) {
+                        // Pause playback because your Audio Focus was temporarily stolen, but will be back soon. i.e. for a phone call
+                        mPlayer.pause();
+                        mPlayer.seekTo(0); // start from the begining
+                    } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+                        // Stop playback, because you lost the Audio Focus.
+                        releaseMediaPlayer();
+                    } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
+                        // Lower the volume or pause, because something else is also playing audio over you.
+                        mPlayer.pause();
+                        mPlayer.seekTo(0); // start from the begining
+                    } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+                        // Resume playback, because you hold the Audio Focus again!
+                        mPlayer.start();
+                    }
+                }
+            };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.word_list);
 
-        final ArrayList<Word> phrases = new ArrayList<Word>();
-        phrases.add(new Word("Where are you going?", "minto wuksus", -1, R.raw.phrase_where_are_you_going));
-        phrases.add(new Word("What is your name?", "tinnə oyaase'nə", -1, R.raw.phrase_what_is_your_name));
-        phrases.add(new Word("My name is...", "oyaaset...", -1, R.raw.phrase_my_name_is));
-        phrases.add(new Word("How are you feeling?", "michəksəs?", -1, R.raw.phrase_how_are_you_feeling));
-        phrases.add(new Word("I’m feeling good.", "kuchi achit", -1, R.raw.phrase_im_feeling_good));
-        phrases.add(new Word("Are you coming?", "əənəs'aa?", -1, R.raw.phrase_are_you_coming));
-        phrases.add(new Word("Yes, I’m coming.", "həə’ əənəm", -1, R.raw.phrase_yes_im_coming));
-        phrases.add(new Word("I’m coming.", "əənəm", -1, R.raw.phrase_im_coming));
-        phrases.add(new Word("Let’s go.", "yoowutis", -1, R.raw.phrase_lets_go));
-        phrases.add(new Word("Come here.", "ənni'nem", -1, R.raw.phrase_come_here));
+        mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
 
-        WordAdapter itemsAdapter = new WordAdapter(this, R.layout.list_item, phrases, R.color.category_phrases);
+        final ArrayList<Word> words = new ArrayList<Word>();
+        words.add(new Word("Where are you going?", "minto wuksus", -1, R.raw.phrase_where_are_you_going));
+        words.add(new Word("What is your name?", "tinnə oyaase'nə", -1, R.raw.phrase_what_is_your_name));
+        words.add(new Word("My name is...", "oyaaset...", -1, R.raw.phrase_my_name_is));
+        words.add(new Word("How are you feeling?", "michəksəs?", -1, R.raw.phrase_how_are_you_feeling));
+        words.add(new Word("I’m feeling good.", "kuchi achit", -1, R.raw.phrase_im_feeling_good));
+        words.add(new Word("Are you coming?", "əənəs'aa?", -1, R.raw.phrase_are_you_coming));
+        words.add(new Word("Yes, I’m coming.", "həə’ əənəm", -1, R.raw.phrase_yes_im_coming));
+        words.add(new Word("I’m coming.", "əənəm", -1, R.raw.phrase_im_coming));
+        words.add(new Word("Let’s go.", "yoowutis", -1, R.raw.phrase_lets_go));
+        words.add(new Word("Come here.", "ənni'nem", -1, R.raw.phrase_come_here));
+
+        WordAdapter itemsAdapter = new WordAdapter(this, R.layout.list_item, words, R.color.category_phrases);
         ListView listView = (ListView) findViewById(R.id.list);
         listView.setAdapter(itemsAdapter);
 
@@ -44,9 +69,19 @@ public class PhrasesActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 releaseMediaPlayer();
-                mPlayer = MediaPlayer.create(PhrasesActivity.this, phrases.get(position).getAudioResourceId());
-                mPlayer.start();
-                mPlayer.setOnCompletionListener(mCompletionListener);
+                // Request audio focus for playback
+                int result = mAudioManager.requestAudioFocus(mOnAudioFocusChangeListener,
+                        // Use the music stream.
+                        AudioManager.STREAM_MUSIC,
+                        // Request temporary focus.
+                        AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+
+                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                    // Start playback
+                    mPlayer = MediaPlayer.create(PhrasesActivity.this, words.get(position).getAudioResourceId());
+                    mPlayer.start();
+                    mPlayer.setOnCompletionListener(mCompletionListener);
+                }
             }
         });
     }
@@ -55,6 +90,14 @@ public class PhrasesActivity extends AppCompatActivity {
         if(mPlayer != null){
             mPlayer.release();
             mPlayer = null;
+            mAudioManager.abandonAudioFocus(mOnAudioFocusChangeListener); // abandon focus
         }
+    }
+
+    @Override
+    protected void onStop() {
+        // app is hide
+        super.onStop();
+        releaseMediaPlayer();
     }
 }
